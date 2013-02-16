9c1d9143f4bf1add8452e94b882475dc5a418f50
Jose Diaz <jose.diaz@joedayz.pe>
2013-02-16 16:07:17 -0500

    demo TiBountyHounterLab

---
+++ TiBountyHunterLab/Resources/app.js
@@ -0,0 +1,23 @@
+
+
+
+
+
+(function(){
+
+	//determinar la plataforma y altura y ancho del dispositivo
+	var osname = Ti.Platform.osname,
+		version = Ti.Platform.version,
+		height = Ti.Platform.displayCaps.platformHeight,
+		width = Ti.Platform.displayCaps.platformWidth;
+		
+
+	Ti.API.info('Welcome to TiBountyHunter for '+ osname);
+	
+	//considerar tabletas
+	var isTablet = osname == 'ipad' || (osname === 'android' 
+			&& (width > 899 || height > 899));
+	
+	
+	
+})();
